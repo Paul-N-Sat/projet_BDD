413e9c20e54631d5ab39b0d4ab23dbdcc873bff3
Paul SCHELLER <pschelle@enssat.fr>
2024-01-11 11:43:39 +0100

    Modif affichageContenus

--- affichageContenu.sql
+++ affichageContenu.sql
@@ -1,12 +1,22 @@
+USE Bibliotheque;
 DELIMITER //
 
 CREATE PROCEDURE affichageContenu(IN keyWord VARCHAR(55))
 BEGIN
-    SELECT * FROM Contenu
+    SELECT * FROM Contenus
     WHERE titre LIKE CONCAT('%', keyWord, '%') 
     OR auteur LIKE CONCAT('%', keyWord, '%') 
     OR type_contenu LIKE CONCAT('%', keyWord, '%') 
     OR genre LIKE CONCAT('%', keyWord, '%');
+
+    IF (SELECT COUNT(*) FROM Contenus
+    WHERE titre LIKE CONCAT('%', keyWord, '%') 
+    OR auteur LIKE CONCAT('%', keyWord, '%') 
+    OR type_contenu LIKE CONCAT('%', keyWord, '%') 
+    OR genre LIKE CONCAT('%', keyWord, '%');) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = "Aucun exemplaire existant. Vérifier l'écriture du mot clé.";
+    END IF 
 END //
 
 DELIMITER ;
